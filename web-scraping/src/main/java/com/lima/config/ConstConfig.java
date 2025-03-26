87eb09f9a6f54816367cb0288e8228674f0b154b
GabrielLima2803 <gabriellima2803@gmail.com>
2025-03-26 20:59:44 -0300

    feat: Configurando Arquivo das Constantes

---
+++ web-scraping/src/main/java/com/lima/config/ConstConfig.java
@@ -0,0 +1,10 @@
+package com.lima.config;
+
+public class ConstConfig {
+    public static final String URL_SITE = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
+    public static final String USER_AGENT = 
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+    "(KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36";
+    public static final String DOWNLOAD_FOLDER = "downloads/";
+    public static final String ZIP_FILE_NAME = "anexos.zip";
+}
